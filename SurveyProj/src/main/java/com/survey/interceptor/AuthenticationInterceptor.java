8ac5c76a6c5db0e0fca09f60a213961c4575fd16
choedong-yong <choedong-yong@192.168.0.3>
2018-04-15 19:00:28 +0900

    인증 인터셉터 클래스 생성
    인증 됬을때만 페이지 이동가능 하도록 코드설

--- SurveyProj/src/main/java/com/survey/interceptor/AuthenticationInterceptor.java
+++ SurveyProj/src/main/java/com/survey/interceptor/AuthenticationInterceptor.java
@@ -1,8 +1,28 @@
 package com.survey.interceptor;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class AuthenticationInterceptor extends HandlerInterceptorAdapter{
+
+	
+	// 컨트롤러에 접근하기 전에 먼저 실행되는 메서드
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		return super.preHandle(request, response, handler);
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		super.postHandle(request, response, handler, modelAndView);
+	}
 
 	
 }
