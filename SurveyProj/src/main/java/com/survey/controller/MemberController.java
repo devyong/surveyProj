45805e8142877f5cf57d8040e1fe2d97b662ae2c
SIST182 <SIST182@SIST182>
2018-04-17 12:06:59 +0900

    controller에 ajax 처리 적용

--- SurveyProj/src/main/java/com/survey/controller/MemberController.java
+++ SurveyProj/src/main/java/com/survey/controller/MemberController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.survey.domain.MemberVO;
 import com.survey.service.MemberService;
 
@@ -35,5 +36,24 @@ public class MemberController {
 		
 		// 회원가입 후 로그인 페이지로
 		return "redirect:/";
+	}
+	
+	@RequestMapping(value = "/member/checkid", method = RequestMethod.GET)
+	public String checkid(Model model, String id) {
+		String result = "";
+		// 아이디 있는지 비교
+		MemberVO member = service.checkid(id);
+		if (member == null) {
+			result = "가능";
+		} else {
+			result = "불가";
+		}
+		// 결과값 모델에 적용
+		
+		
+		model.addAttribute("data", result);
+		
+		
+		return "ajax.ajax";
 	}
 }
